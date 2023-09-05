404b0b82679ffe95f059e5de38bc91d024122d88
Ahmed Raza <bhaia9036@gmail.com>
2023-09-05 16:50:46 +0500

    Last Changes>

--- index.js
+++ index.js
@@ -299,47 +299,47 @@ app.post('/login', async (req, res) => {
     res.status(500).send({ message: "login failed, please try later" });
   }
 });
-app.use('/api/v1', (req, res, next) => {
+// app.use('/api/v1', (req, res, next) => {
 
-  console.log("req.cookies: ", req.cookies.Token);
+//   console.log("req.cookies: ", req.cookies.Token);
 
-  if (!req?.cookies?.Token) {
-    res.status(401).send({
-      message: "include http-only credentials with every request"
-    })
-    return;
-  }
+//   if (!req?.cookies?.Token) {
+//     res.status(401).send({
+//       message: "include http-only credentials with every request"
+//     })
+//     return;
+//   }
 
-  jwt.verify(req.cookies.Token, SECRET, function (err, decodedData) {
-    if (!err) {
+//   jwt.verify(req.cookies.Token, SECRET, function (err, decodedData) {
+//     if (!err) {
 
-      console.log("decodedData: ", decodedData);
+//       console.log("decodedData: ", decodedData);
 
-      const nowDate = new Date().getTime() / 1000;
+//       const nowDate = new Date().getTime() / 1000;
 
-      if (decodedData.exp < nowDate) {
+//       if (decodedData.exp < nowDate) {
 
-        res.status(401);
-        res.cookie('Token', '', {
-          maxAge: 1,
-          httpOnly: true,
-          sameSite: 'none',
-          secure: true
-        });
-        res.send({ message: "token expired" })
+//         res.status(401);
+//         res.cookie('Token', '', {
+//           maxAge: 1,
+//           httpOnly: true,
+//           sameSite: 'none',
+//           secure: true
+//         });
+//         res.send({ message: "token expired" })
 
-      } else {
+//       } else {
 
-        console.log("token approved");
+//         console.log("token approved");
 
-        req.body.token = decodedData
-        next();
-      }
-    } else {
-      res.status(401).send("invalid token")
-    }
-  });
-})
+//         req.body.token = decodedData
+//         next();
+//       }
+//     } else {
+//       res.status(401).send("invalid token")
+//     }
+//   });
+// })
 app.get('/api/v1/profile', (req, res) => {
   const _id = req.body.token._id
   const getData = async () => {
