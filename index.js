6ce8b84a7da588fe7af4811038794cc42893a8c4
brand-steps <bhaia9036@gmail.com>
2023-11-20 19:05:19 +0500

    first commit

--- index.js
+++ index.js
@@ -19,12 +19,8 @@ import { managerModel } from "./Models/User.js";
 import { mentorModel } from "./Models/User.js";
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "*"],
-    credentials: true,
-  })
-);
+app.use(cors({origin: true, credentials: true}));
+
 const storage = multer.diskStorage({
   destination: "/tmp",
   filename: function (req, file, cb) {
